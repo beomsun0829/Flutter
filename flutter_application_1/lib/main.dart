095f74d98932b79d9b7fdd2c3ac2313469a31bd8
Hong Beomsun <ghsqjatns@naver.com>
2021-12-28 22:26:55 +0900

    main update

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:ui';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,56 +12,61 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //final wordPair = WordPair.random();
 
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
+
+      title: 'BBANTO',
+      home: Grade(),
+
     );
+
+    
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+class Grade extends StatelessWidget {
+  const Grade({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //final WordPair wordPair = WordPair.random();
-    //return Text(wordPair.asPascalCase);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
+      backgroundColor: Colors.amber[800],
+      appBar : AppBar(
+        title: Text('BBANTO'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
       ),
-      body: _buildSuggestions(),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+        child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: <Widget>[
+            Text('NAME',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+              ),
+            ),
+
+            SizedBox(
+              height: 30.0,
+            ),
+
+
+            Text('BBANTO',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )
+      )
     );
   }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) return Divider();
-
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-        title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ));
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
 }
